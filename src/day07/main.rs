8c617d5ca70e4434898e6db31b2633e3390a5f9a
Ben Ibinson <ben@ibinson.dev>
2022-12-07 22:21:19 +0000

    Fmt

--- src/day07/main.rs
+++ src/day07/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, collections::HashMap};
+use std::{collections::HashMap, fs};
 
 enum Cmd<'a> {
     CdRoot,
@@ -6,17 +6,17 @@ enum Cmd<'a> {
     Cd(&'a str),
     Ls,
     Dir(&'a str),
-    File(&'a str, u32)
+    File(&'a str, u32),
 }
 
 impl Cmd<'_> {
-    fn parse<'a>(line: &'a str) -> Cmd<'a>{
+    fn parse<'a>(line: &'a str) -> Cmd<'a> {
         if line.starts_with("$ cd") {
             let dir = line.strip_prefix("$ cd ").unwrap();
             match dir {
                 "/" => return Cmd::CdRoot,
                 ".." => return Cmd::CdUp,
-                _ => return Cmd::Cd(dir)
+                _ => return Cmd::Cd(dir),
             }
         } else if line == "$ ls" {
             return Cmd::Ls;
@@ -24,7 +24,7 @@ impl Cmd<'_> {
             return Cmd::Dir(line.strip_prefix("dir ").unwrap());
         } else {
             let (size_str, filename) = line.split_once(" ").unwrap();
-            return Cmd::File(filename, size_str.parse().unwrap())
+            return Cmd::File(filename, size_str.parse().unwrap());
         }
     }
 }
@@ -37,7 +37,11 @@ struct DirWalker<'a> {
 
 impl DirWalker<'_> {
     fn new<'a>(cmds: Vec<Cmd<'a>>) -> DirWalker<'a> {
-        DirWalker { cmds: cmds, cwd: vec!["/"], dir_sizes: HashMap::new() }
+        DirWalker {
+            cmds: cmds,
+            cwd: vec!["/"],
+            dir_sizes: HashMap::new(),
+        }
     }
 
     fn execute(&mut self) {
@@ -48,12 +52,12 @@ impl DirWalker<'_> {
                 Cmd::Cd(dir) => self.cwd.push(dir),
                 Cmd::Dir(_) => (),
                 Cmd::Ls => (),
-                Cmd::File(_,size) => DirWalker::add_file(&self.cwd, &mut self.dir_sizes, *size)
+                Cmd::File(_, size) => DirWalker::add_file(&self.cwd, &mut self.dir_sizes, *size),
             }
-        };
+        }
     }
 
-    fn add_file(cwd: &Vec<&str>, dir_sizes: &mut HashMap<String, u32> , file_size: u32) {
+    fn add_file(cwd: &Vec<&str>, dir_sizes: &mut HashMap<String, u32>, file_size: u32) {
         // Starting at the root directory, as the size to all containing folders
         let mut path = String::from("");
         cwd.iter().for_each(|x| {
@@ -61,7 +65,8 @@ impl DirWalker<'_> {
                 path.push_str("/");
             }
             path.push_str(x);
-            dir_sizes.entry(path.to_owned())
+            dir_sizes
+                .entry(path.to_owned())
                 .and_modify(|y| *y += file_size)
                 .or_insert(file_size);
         })
@@ -78,20 +83,24 @@ fn main() {
     let mut dir_walker = DirWalker::new(input);
     dir_walker.execute();
 
-    let total: u32 = dir_walker.dir_sizes.values()
-        .filter(|size| **size <= 100000 )
+    let total: u32 = dir_walker
+        .dir_sizes
+        .values()
+        .filter(|size| **size <= 100000)
         .sum();
 
     println!("Part 1 : Total {}", total);
 
     const DISK_SIZE: u32 = 70000000;
     const REQ_SIZE: u32 = 30000000;
-    let used_size =  dir_walker.dir_sizes.get("/").unwrap();
+    let used_size = dir_walker.dir_sizes.get("/").unwrap();
     let free_size = DISK_SIZE - used_size;
     let need_to_free = REQ_SIZE - free_size;
 
-    let min_size =  dir_walker.dir_sizes.values()
-        .filter(| size| **size > need_to_free)
+    let min_size = dir_walker
+        .dir_sizes
+        .values()
+        .filter(|size| **size > need_to_free)
         .min()
         .unwrap();
 
